c2808be357761b59478a069981f45182bf47c347
Joyce Apagyiwa Inkoom <joycerhodainkoom2000@gmail.com>
2024-10-02 15:24:26 +0000

    Todo app using react.js

---
+++ src/pages/AddTodo.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
+const AddTodo = () => {
+  const navigate = useNavigate();
+
+  const saveTodo = (event) => {
+    event.preventDefault();
+    // post data to todo api
+    // goto the homepage
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <h1>Add A New Todo</h1>
+      <form onSubmit={saveTodo}>
+        <input type="text" placeholder="Enter Task" required />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+export default AddTodo;
